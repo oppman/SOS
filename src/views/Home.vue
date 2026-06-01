885c6d297b5742b4a894f856957cf04d73fc6875
oppman <ndekeziopportun96@gmail.com>
2026-06-01 16:12:03 +0200

    la masia

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,7 +5,7 @@
 
     <section
       class="relative h-screen bg-cover bg-center"
-      style="background-image: url(/src/assets/images/homebackground.jpg)"
+      style="background-image: url(/src/assets/images/gorilla.jpg)"
     >
       <div class="absolute inset-0 bg-black/60"></div>
 
